858d0ec8a1dad6c02be269c20acfc5f7cdf11666
Gittyup191 <karcheski.jason@gmail.com>
2025-02-25 22:20:46 -0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,35 @@
+# Screen settings
+WIDTH, HEIGHT = 800, 600
+FPS = 60
+
+# Colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+ORANGE = (255, 165, 0)
+YELLOW = (255, 255, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+PURPLE = (128, 0, 128)
+
+# Paddle settings
+PADDLE_WIDTH = 100
+PADDLE_HEIGHT = 20
+PADDLE_SPEED = 10
+
+# Ball settings
+BALL_RADIUS = 10
+BALL_SPEED = 5
+
+# Brick settings
+BRICK_WIDTH = 70
+BRICK_HEIGHT = 30
+BRICK_ROWS = 6
+BRICK_COLS = WIDTH // BRICK_WIDTH
+
+# Power-ups
+POWERUP_WIDTH = 20
+POWERUP_HEIGHT = 20
+
+# Game settings
+LIVES = 3
